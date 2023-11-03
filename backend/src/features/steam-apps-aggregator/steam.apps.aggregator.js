b0a3c85cf4ef1060a872c23d6a45dfdd0944d26c
Luka Tarman <luka.tarman@gmail.com>
2023-11-03 11:06:10 +0100

    fixed bug

--- backend/src/features/steam-apps-aggregator/steam.apps.aggregator.js
+++ backend/src/features/steam-apps-aggregator/steam.apps.aggregator.js
@@ -30,7 +30,7 @@ export class SteamAppsAggregator {
       return;
     }
 
-    const x = this.#options.runner.options.defaultIntervalDelay;
+    const x = this.#options.features.options.updateIntervalDelay;
     if (moreThanXhoursPassedSince(x, lastUpdate.updatedOn))
       await this.#collectSteamApps();
   };
